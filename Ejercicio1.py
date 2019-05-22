1f027aa54804239293f5fb3794f96a703b41cdb1
SelePao13 <50873012+SelePao13@users.noreply.github.com>
2019-05-22 08:44:54 -0500

    Delete Ejercicio1.py

--- Ejercicio1.py
+++
@@ -1,9 +0,0 @@
-primos = [2]
-numero_max = 1000
-for x in range (2,numero_max):
-    for i in range (2,x):
-        if x%i !=0:
-            continue
-        else:
-            break
-    else: print ('%d'%x)
